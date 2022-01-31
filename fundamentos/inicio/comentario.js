22757737dd9e755ab0482c6ca96498e30e51529e
Lucian <lucian.bonifacio@gmail.com>
2022-01-31 15:49:58 -0300

    assisti 3 aulas hoje

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,14 @@
+// console.log é usado para exibir algo na tela do computador
+console.log('Passo  #1')
+console.log('Passo  #2') // mais um exemplto de console.log
+// console.log('Passo  #3')
+
+/*
+    Esse é um 
+    comentário de 
+    múltiplas 
+    linhas!
+*/
+console.log('Passo  #3')
+
+
