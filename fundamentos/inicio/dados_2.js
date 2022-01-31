5d27f1cbaae5556ed5d491a201e63c18f76d3e50
Lucian <lucian.bonifacio@gmail.com>
2022-01-31 18:12:20 -0300

    mais aulas assistidas

---
+++ fundamentos/inicio/dados_2.js
@@ -0,0 +1,12 @@
+let preco = 19.90
+let desconto = 0.4
+
+console.log(19.90 * 0.6)
+
+let precoComDesconto = preco * (1 - desconto)
+console.log(precoComDesconto)
+
+let nome = 'Caderno'
+let categoria = 'Papelaria'
+
+console.log(nome + ' ' + categoria)
