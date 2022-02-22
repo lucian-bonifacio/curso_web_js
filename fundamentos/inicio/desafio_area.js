3c2c31ccd109a47d953bbadba1cc397ffa36fc6a
Lucian <lucian.bonifacio@gmail.com>
2022-02-22 11:21:05 -0300

    aulas do dia 22/02/2022. Ops, data mística!

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592
+let raio = 10
+
+let area = PI * (raio * raio)
+
+console.log('O valor da área é ' + area + ' m2.')
+
+console.log(Math.PI)
---
+++ fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7
+let b = 94
+
+// depois da troca.. a = 94 e b = 7
+
+let c = 0
+
+c = a
+a = b
+b = c
+
+
+console.log(a)
+console.log(b)
